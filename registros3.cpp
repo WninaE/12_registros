dbdfe1df69c4e21e10b8baba0fd0f2a214090262
WninaE <wninae@unjbg.edu.pe>
2024-07-13 21:35:12 -0500

    Actualización registros3.cpp: Proceso y salidas.

--- registros3.cpp
+++ registros3.cpp
@@ -23,6 +23,20 @@ int main(){
 		cout<<"Mes de nacimiento: "; cin>>p.mes_de_nacimiento[i];
 		cout<<"Anio de nacimiento: "; cin>>p.anio_de_nacimiento[i];
 	}
+	
+	do{
+		cout<<endl<<endl<<"Ingrese un mes: "; cin>>m;
+		cout<<"Las personas que cumplen años en el mes "<<m<<" son:";
+		for(int j=0; j<n;j++){
+			if(p.mes_de_nacimiento[j]==m){
+				cout<<endl<<endl<<"Persona "<<j+1<<endl;
+				cout<<"Nombre: "<<p.nombre[j];
+				cout<<endl<<"Dia de nacimiento: "<<p.dia_de_nacimiento[j];
+				cout<<endl<<"Mes de nacimiento: "<<p.mes_de_nacimiento[j];
+				cout<<endl<<"Anio de nacimiento: "<<p.anio_de_nacimiento[j];
+			}
+		}
+	} while(m!=0);
 	system("pause");
 	return 0;
 }
