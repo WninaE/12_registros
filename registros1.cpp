ee0c705a8ecb057950497994b1f6260ec16ba8cb
WninaE <wninae@unjbg.edu.pe>
2024-07-13 20:47:55 -0500

    Actualización de registros1.cpp: Proceso y salidas.

--- registros1.cpp
+++ registros1.cpp
@@ -22,7 +22,27 @@ int main(){
 		cout<<"Nombre: "; cin>>E.nombre[i];
 		cout<<"Sexo: "; cin>>E.sexo[i];
 		cout<<"Sueldo: "; cin>>E.sueldo[i];
-	]
+		if(E.sueldo[i]>SMa){
+			SMa=E.sueldo[i];
+		}
+		if(E.sueldo[i]<SMe){
+			SMe=E.sueldo[i];
+		}
+	}
+	for(int j=0; j<n; j++){
+		if(E.sueldo[j]==SMa){
+			cout<<endl<<"Datos del empleado con mayor sueldo";
+			cout<<endl<<"Nombre: "<<E.nombre[j];
+			cout<<endl<<"Sexo: "<<E.sexo[j];
+			cout<<endl<<"Sueldo: "<<E.sueldo[j];
+		}
+		if(E.sueldo[j]==SMe){
+			cout<<endl<<endl<<"Datos del empleado con menor sueldo";
+			cout<<endl<<"Nombre: "<<E.nombre[j];
+			cout<<endl<<"Sexo: "<<E.sexo[j];
+			cout<<endl<<"Sueldo: "<<E.sueldo[j];
+		}
+	}
 	system("pause");
 	return 0;
 }
