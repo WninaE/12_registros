652a9a3b06799c23ae888fea7a72adbeea265912
WninaE <wninae@unjbg.edu.pe>
2024-07-13 22:01:19 -0500

    Actualización registros4.cpp: Proceso y salidas.

--- registros4.cpp
+++ registros4.cpp
@@ -22,6 +22,31 @@ int main(){
 		cout<<"Disciplina: "; cin>>A.disciplina[i];
 		cout<<"Numero de medallas: "; cin>>A.numero_de_medallas[i];
 	}
+	string p; int MNM=0;
+	cout<<endl<<endl<<"Ingrese un pais: "; cin>>p;
+	cout<<endl<<"Los atletas que son del pais "<<p<<" son: ";
+	for(int j=0; j<n; j++){
+		if(A.pais[j]==p){
+			cout<<endl<<"Atleta "<<j+1;
+			cout<<endl<<"Nombres: "<<A.nombres[j];
+			cout<<endl<<"Pais: "<<A.pais[j];
+			cout<<endl<<"Disciplina: "<<A.disciplina[j];
+			cout<<endl<<"Numero de medallas: "<<A.numero_de_medallas[j];
+			if(A.numero_de_medallas[j]>MNM){
+				MNM=A.numero_de_medallas[j];
+			}
+		}
+	}
+	cout<<endl<<endl<<"El atleta con mayor medallas del pais de "<<p<<" es: ";
+	for(int k=0; k<n; k++){
+		if(A.numero_de_medallas[k]==MNM){
+			cout<<endl<<"Atleta "<<k+1;
+			cout<<endl<<"Nombres: "<<A.nombres[k];
+			cout<<endl<<"Pais: "<<A.pais[k];
+			cout<<endl<<"Disciplina: "<<A.disciplina[k];
+			cout<<endl<<"Numero de medallas: "<<A.numero_de_medallas[k];
+		}
+	}
 	system("pause");
 	return 0;
 }
